b61c5c374a0c369ea1e30dd375cf139f6f2a02c9
belskiyroman <belskiyroman@gmail.com>
2018-08-03 20:20:23 +0300

    Add patient schema for generate seeds.

--- src/core/sagas/physician.saga.js
+++ src/core/sagas/physician.saga.js
@@ -3,16 +3,21 @@ import {
 } from 'redux-saga/effects';
 import { PATIENT_LIST_REQUEST } from '../types';
 import { putPatientListAction } from '../actions';
+import { PAGINATION_BUTCH_SIZE } from '../constants';
 import { Api } from '../../utils';
 
 function* physicianSaga() {
   while (true) {
     try {
-      yield take(PATIENT_LIST_REQUEST);
+      const { payload = {} } = yield take(PATIENT_LIST_REQUEST);
       const isPatientListLoaded = yield select(state => state.physician.isPatientListLoaded);
+      const { pagination } = yield select(state => state.physician.patientListMeta);
 
       if (!isPatientListLoaded) {
-        const data = yield call(Api.patientList);
+        const data = yield call(Api.patientList, {
+          offset: payload.offset || pagination.offset,
+          batch_size: PAGINATION_BUTCH_SIZE,
+        });
         yield put(putPatientListAction(data));
       }
     } catch (error) {
